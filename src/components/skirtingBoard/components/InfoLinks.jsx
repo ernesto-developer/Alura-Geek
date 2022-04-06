196a8a7136765cc925d36c075cbe69e3156e4025
Ernesto Ruiz <ernesto_711@live.com.mx>
2022-04-06 15:59:02 -0500

    add app splashImages for future implementations

--- src/components/skirtingBoard/components/InfoLinks.jsx
+++ src/components/skirtingBoard/components/InfoLinks.jsx
@@ -3,19 +3,18 @@ import {  NavLink } from 'react-router-dom'
 import { contactLinks } from '../../../resources/contactLinks'
 
 export const InfoLinks = () => {
-
   return (
-  
+
       <ul className='skirtingBoard__container__links' >  
-          {contactLinks.map((link, index) => {
-            return (
+
+          {contactLinks.map((link, index) => 
                 <NavLink 
                     key={index} 
                     className='skirtingBoard__container__links-link' 
                     to={`/login`}>{link}
                 </NavLink>
-            );
-          })} 
+          )} 
+
       </ul>
  
   )
